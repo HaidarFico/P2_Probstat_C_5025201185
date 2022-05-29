70a10afd94d691897ec36753e2627e93dceaac1d
HaidarFico <haidarficoi@gmail.com>
2022-05-29 23:10:29 +0700

    First working version

--- C_5025201185.R
+++ C_5025201185.R
@@ -24,4 +24,97 @@ t.test(Table$y - Table$x, alternative = "two.sided", var.equal = TRUE)
 # 2 b
 tsum.test(mean.x=23500, sd(3900), n.x=100)
 
-# 2 c terdapat pada README
+# 2 c
+# Find z value
+zValueSecond = (23500-20000)/(3900/sqrt(100))
+2 * pnorm(-abs(zValueSecond))
+
+# Soal 3
+# 3 a 
+h0 = (3.64)/(1.67/sqrt(19))
+h1 = (2.79)/(1.32/sqrt(27))
+h0
+h1
+
+# 3 b
+tsum.test(mean.x=3.64, s.x = 1.67, n.x = 19, mean.y =2.79 , s.y = 1.32, n.y = 27, alternative = "greater", var.equal = TRUE)
+
+# 3 c
+install.packages("mosaic")
+library(mosaic)
+plotDist(dist='t', df=2, col="blue")
+
+# 3 d
+qchisq(p = 0.05, df = 2, lower.tail = FALSE)
+
+# 3 e
+# Terdapat di README
+
+# 3 f
+# Terdapat di README
+
+# Soal 4
+# 4 a
+
+tableDownload  <- read.table(url("https://rstatisticsandresearch.weebly.com/uploads/1/0/2/6/1026585/onewayanova.txt"))
+dim(tableDownload)
+head(tableDownload)
+attach(tableDownload)
+
+tableDownload$V1 <- as.factor(tableDownload$V1)
+tableDownload$V1 = factor(tableDownload$V1,labels = c("Kucing Oren","Kucing Hitam","Kucing Putih","Kucing Oren"))
+
+class(tableDownload$V1)
+
+firstGroup <- subset(tableDownload, V1=="Kucing Oren")
+secondGroup <- subset(tableDownload, V1=="Kucing Hitam")
+thirdGroup <- subset(tableDownload, V1=="Kucing Putih")
+
+# 4 b
+
+bartlett.test(Length~Group, data=dataoneway)
+
+# Soal 5
+# 5 a
+# Soal 5 a
+install.packages("multcompView")
+library(dplyr)
+library(ggplot2)
+library(readr)
+library(multcompView)
+
+
+GTLImport <- read_csv("GTL.csv")
+head(GTLImport)
+str(GTL)
+
+qplot(x = Temp, y = Light, geom = "point", data = GTLImport) +
+  facet_grid(.~Glass, labeller = label_both)
+
+# Soal 5 b
+GTL$Glass <- as.factor(GTLImport$Glass)
+GTL$Temp_Factor <- as.factor(GTLImport$Temp)
+str(GTLImport)
+
+anova <- aov(Light ~ Glass*Temp_Factor, data = GTLImport)
+summary(anova)
+
+# Soal 5 c
+data_summary <- group_by(GTLImport, Glass, Temp) %>%
+  summarise(mean=mean(Light), sd=sd(Light)) %>%
+  arrange(desc(mean))
+print(data_summary)
+
+# Soal 5 d
+tukeyRes <- TukeyHSD(anova)
+tukeyRes
+
+# Soal 5 e
+tukeyCLD <- multcompLetters4(anova, tukey)
+print(tukeyCLD)
+
+CLD <- as.data.frame.list(tukeyCLD$`Glass:Temp_Factor`)
+data_summary$tukeyCLD <- CLD$Letters
+print(data_summary)
+
+write.csv("GTL_summary.csv")
